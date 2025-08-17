b4b4278d93d517cd88612783bc7cca29bb9c0687
yasmine bounasla <yasminebounasla@gmail.com>
2025-08-17 18:10:24 +0200

    finish frontend part

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+
+export default function Header () {
+    return (
+        <header>
+            <img src="/globe.png" alt="globe-icone"/>
+            <h1>my travel journal</h1>
+        </header>
+    )
+}
